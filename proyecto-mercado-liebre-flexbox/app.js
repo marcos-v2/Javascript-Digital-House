4eeae3d2eaeeabb3cbf3b4c7fc87da755fa6ce99
marcos-v2 <marcos.h.vicente@gmail.com>
2022-03-29 23:55:06 -0300

    un poco de formularios en la implementación de mercado Liebre. Header arreglado

--- proyecto-mercado-liebre-flexbox/app.js
+++ proyecto-mercado-liebre-flexbox/app.js
@@ -10,3 +10,9 @@ app.listen(3000, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
