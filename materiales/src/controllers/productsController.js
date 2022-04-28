6e580c33ac4f7ff49bbdf38ec560d68030fb2755
Marcos Vicente <100540306+marcos-v2@users.noreply.github.com>
2022-04-27 22:25:47 -0300

    get de todos los productos y vista de todos los productos y detalle de producto

--- materiales/src/controllers/productsController.js
+++ materiales/src/controllers/productsController.js
@@ -9,6 +9,7 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
+		res.render("products",{products})
 		// Do the magic
 	},
 
