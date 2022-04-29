2f238e78c9fd49e8c232a98e12bca0cba8c25c60
Marcos Vicente <100540306+marcos-v2@users.noreply.github.com>
2022-04-28 23:27:27 -0300

    CRUD episodio 2

---
+++ CRUD-episodio-2/materiales/src/controllers/productsController.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		res.render('products');
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		res.render('detail');
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		
+		res.render('product-create-form');
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		const newProduct_json=JSON.stringify(req.body);
+		const newProduct=JSON.parse(newProduct_json);
+		
+		// Do the magic
+		newProduct.id=products.length +1;
+		products.push(newProduct);
+		res.redirect("/products");
+		const newData=JSON.stringify(products);
+		// 
+		fs.writeFileSync(path.join(__dirname,"../data/productsDataBase.json"),newData)
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		res.render('product-edit-form');
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		// Do the magic
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		// Do the magic
+	}
+};
+
+module.exports = controller;
