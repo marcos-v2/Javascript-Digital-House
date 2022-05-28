50999749535cb8dc98b8b6879553c28995c19eee
Marcos Vicente <100540306+marcos-v2@users.noreply.github.com>
2022-05-28 17:19:48 -0300

    microdesafío Sequelize
    Movies, Genres (Sin bonus track de actores)

---
+++ Practica-sequelize/materiales/src/controllers/genresController.js
@@ -0,0 +1,8 @@
+let db = require("../database/models");
+module.exports={
+    list:(req,res)=>{db.Genres.findAll()
+    .then ((genres)=>{res.render("genresList",{genres:genres})})},
+    detail:(req,res)=>{db.Genres.findByPk(req.params.id)
+    .then ((genre)=>{res.render("GenresDetail",{genre:genre})})},
+   
+}
--- Practica-sequelize/materiales/src/controllers/moviesController.js
+++ Practica-sequelize/materiales/src/controllers/moviesController.js
@@ -1,5 +1,13 @@
 let db = require("../database/models");
 module.exports={
     list:(req,res)=>{db.Movies.findAll()
-    .then ((movies)=>{res.render("MoviesList",{movies:movies})})}
+    .then ((movies)=>{res.render("moviesList",{movies:movies})})},
+    detail:(req,res)=>{db.Movies.findByPk(req.params.id)
+    .then ((movie)=>{res.render("moviesDetail",{movie:movie})})},
+    new:(req,res)=>{
+        db.Movies.findAll({order:[["release_date","DESC"]]})
+        .then((newest) =>{res.render("newestMovies",{movies:newest})})}
+        ,    recommended:(req,res)=>{
+        db.Movies.findAll({order:[["rating","DESC"]],limit:5})
+        .then((recommendations) =>{res.render("recommendedMovies",{movies:recommendations})})}
 }
